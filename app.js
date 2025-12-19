2f21f1482b7d739a1f71e8a21ca23d9ec9f5af2d
Jharmez007 <jamesagu2001@gmail.com>
2025-12-19 02:46:36 +0100

    initial website setup

---
+++ app.js
@@ -0,0 +1,15 @@
+let age = 17;
+
+if (age >= 18) {
+  console.log("You are an adult!");
+} else {
+  console.log("You are a minor!");
+}
+
+let name ="james";
+
+if (name == "Nnamdi") {
+    console.log(name);
+} else {
+    console.log("your name is", name)
+}
